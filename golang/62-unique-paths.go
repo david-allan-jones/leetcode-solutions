65e4e6213fe7af09609094416d779b666ae7f4a1
david-allan-jones <davej1094@yahoo.com>
2022-08-09 10:00:43 -0500

    improve unique paths solutions for go and javascript

--- golang/62-unique-paths.go
+++ golang/62-unique-paths.go
@@ -1,20 +1,15 @@
 package golang
 
 func uniquePaths(m int, n int) int {
-	dp := make([][]int, m)
-	for i, _ := range dp {
-		dp[i] = make([]int, n)
+	r, result := m+n-2, 1
+	var min int
+	if m < n {
+		min = m - 1
+	} else {
+		min = n - 1
 	}
-
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
-			if i == 0 || j == 0 {
-				dp[i][j] = 1
-			} else {
-				dp[i][j] = dp[i-1][j] + dp[i][j-1]
-			}
-		}
+	for k := 0; k < min; k++ {
+		result = (result * (r - k)) / (k + 1)
 	}
-
-	return dp[m-1][n-1]
+	return result
 }
