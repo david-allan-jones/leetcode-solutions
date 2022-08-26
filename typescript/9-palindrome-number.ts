781dfe4d16ec48e68a1eb4274df5e2981f1a321b
david-allan-jones <davej1094@yahoo.com>
2022-08-26 15:34:19 -0500

    typescript question 9

---
+++ typescript/9-palindrome-number.ts
@@ -0,0 +1,10 @@
+function isPalindrome(x: number): boolean {
+    if (x < 0) return false;
+    let t = x, r = 0, m = 0;
+    while (t > 0) {
+        m = t % 10;
+        r = r * 10 + m;
+        t = Math.floor(t/10);
+    }
+    return r === x;
+};
