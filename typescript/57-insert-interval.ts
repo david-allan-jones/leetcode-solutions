51bd0ac3d7b5cb49e7b3eb634f8649c6309836e0
david-allan-jones <davej1094@yahoo.com>
2022-08-29 21:10:11 -0500

    typescript question 57

---
+++ typescript/57-insert-interval.ts
@@ -0,0 +1,15 @@
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+    const result: number[][] = []
+    for (let i = 0; i < intervals.length; i++) {
+        if (newInterval[0] > intervals[i][1]) {
+            result.push(intervals[i])
+        } else if (newInterval[1] < intervals[i][0]) {
+            result.push(newInterval)
+            return result.concat(intervals.slice(i))
+        } else {
+            newInterval = [Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])]
+        }
+    }
+    result.push(newInterval)
+    return result
+};
