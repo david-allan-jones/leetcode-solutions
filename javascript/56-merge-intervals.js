bfdb8a140de4e7878c6d123b41aa2e7ee889cab0
david-allan-jones <davej1094@yahoo.com>
2022-08-30 21:07:55 -0500

    javascript question 56

---
+++ javascript/56-merge-intervals.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+ var merge = function(intervals) {
+    // sort by start
+    intervals.sort((i1, i2) => i1[0] - i2[0])
+        
+    const result = []
+    let prev = intervals[0]
+    for (let i = 1; i < intervals.length; i++) {
+        intervals[i]
+        // if the interval overlaps with the previous one, use the max end to create a new interval
+        if (intervals[i][0] <= prev[1]) {
+            prev[1] = Math.max(prev[1], intervals[i][1])
+
+        // otherwise, add the previous interval and reset it to the current one
+        } else {
+            result.push(prev)
+            prev = intervals[i]
+        }
+    }
+    // add the last interval
+    result.push(prev)
+    return result
+};
