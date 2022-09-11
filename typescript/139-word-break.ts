bd1829afaf81576e6777411d0439bbcb9174b401
david-allan-jones <davej1094@yahoo.com>
2022-09-10 21:04:26 -0500

    typescript question 139

---
+++ typescript/139-word-break.ts
@@ -0,0 +1,15 @@
+function wordBreak(s: string, wordDict: string[]): boolean {
+    let dp = (new Array(s.length + 1)).fill(false)
+    dp[s.length] = true    
+    for (let i = s.length - 1; i >= 0; i--) {
+        for (const w of wordDict) {
+            if (i + w.length <= s.length && w === s.substring(i, i + w.length)) {
+                dp[i] = dp[i + w.length]
+            }
+            if (dp[i]) {
+                break
+            }
+        }
+    }
+    return dp[0]
+};
