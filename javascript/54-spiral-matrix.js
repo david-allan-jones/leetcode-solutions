861176243ba707cd5e45108405ec314dfa406d6f
david-allan-jones <davej1094@yahoo.com>
2022-09-16 08:20:05 -0500

    javascript question 54

---
+++ javascript/54-spiral-matrix.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+ var spiralOrder = function(matrix) {
+    let m = matrix.length, n = matrix[0].length
+    const r = m * n, result = []
+    let sr = 0, sc = 0, er = matrix.length - 1, ec = matrix[0].length - 1
+    while (result.length < r) {
+        //traverse right
+        for (let i = sc; i <= ec; i++) {
+            result.push(matrix[sr][i])
+        }
+        
+        //traverse down
+        for (let i = sr + 1; i <= er; i++) {
+            result.push(matrix[i][ec])
+        }
+        
+        //traverse left
+        for (let i = ec - 1; i >= sc; i--) {
+            if (sr === er) break
+            result.push(matrix[er][i])
+        }
+        
+        //traverse up
+        for (let i = er - 1; i >= sr + 1; i--) {
+            if (sc === ec) break
+            result.push(matrix[i][sc])
+        } 
+        sr++
+        sc++
+        er--
+        ec--
+    }   
+    return result
+};
