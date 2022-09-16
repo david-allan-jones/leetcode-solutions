d1c98e8c47b7a365d98c954e3989e7f7e3d82a2e
david-allan-jones <davej1094@yahoo.com>
2022-09-16 09:03:25 -0500

    javascript question 412

---
+++ javascript/412-fizz-buzz.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+ var fizzBuzz = function(n) {
+    const result = new Array(n)
+    for (let i = 0; i < n; i++) {
+        let num = i + 1
+        const divisibleByThree = num % 3 === 0
+        const divisibleByFive = num % 5 === 0
+        if (divisibleByThree && divisibleByFive) {
+            result[i] = 'FizzBuzz'
+            continue
+        }
+        if (divisibleByThree) {
+            result[i] = 'Fizz'
+            continue
+        }
+        if (divisibleByFive) {
+            result[i] = 'Buzz'
+            continue
+        }   
+        result[i] = `${num}`
+    }
+    return result
+};
