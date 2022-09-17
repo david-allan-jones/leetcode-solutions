72f2738204e7d4d482b47cb1b8056552f21465ae
david-allan-jones <davej1094@yahoo.com>
2022-09-17 11:16:05 -0500

    sql question 595

--- sql/1741-find-total-time-spent-by-each-employee.sql
+++ sql/1741-find-total-time-spent-by-each-employee.sql
@@ -2,7 +2,8 @@ SELECT
     event_day AS day,
     emp_id,
     SUM(out_time-in_time) AS total_time
-FROM Employees
+FROM
+    Employees
 GROUP BY
     day,
     emp_id
--- sql/1757-recyclable-and-low-fat-products.sql
+++ sql/1757-recyclable-and-low-fat-products.sql
@@ -1,3 +1,6 @@
-SELECT product_id
-FROM Products
-WHERE low_fats='Y' AND recyclable='Y' 
+SELECT
+    product_id
+FROM
+    Products
+WHERE 
+    low_fats='Y' AND recyclable='Y' 
