f2bea95b41725c5cd27a5f3593a771daee33c934
david-allan-jones <davej1094@yahoo.com>
2022-09-17 10:57:24 -0500

    typescript question 48

---
+++ typescript/48-rotate-image.ts
@@ -0,0 +1,17 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+ function rotate(matrix: number[][]): void {
+    let left = 0, right = matrix.length - 1
+    while (left < right) {
+        for (let i = 0; i < right - left; i++) {
+            let tmp = matrix[left][left + i]
+            matrix[left][left + i] = matrix[right - i][left]
+            matrix[right - i][left] = matrix[right][right - i]
+            matrix[right][right - i] = matrix[left + i][right]
+            matrix[left + i][right] = tmp
+        }
+        left++
+        right--
+    }
+};
