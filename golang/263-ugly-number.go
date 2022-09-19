7da4da144ac54b3b2c340f5a0c05ecfcec8b8f98
david-allan-jones <davej1094@yahoo.com>
2022-09-19 13:23:44 -0500

    go question 263

---
+++ golang/263-ugly-number.go
@@ -0,0 +1,17 @@
+package golang
+
+func isUgly(n int) bool {
+    if n <= 0 {
+        return false
+    }
+    for n % 2 == 0 {
+        n = n / 2
+    }
+    for n % 3 == 0 {
+        n = n / 3
+    }
+    for n % 5 == 0 {
+        n = n / 5
+    }
+    return n == 1
+}
