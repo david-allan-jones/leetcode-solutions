b6f19b2d2b69ba3d7aaa55a947b8711a4bf6fcdf
david-allan-jones <davej1094@yahoo.com>
2023-10-01 18:26:33 +0900

    go question 367 update

--- golang/367-valid-perfect-square.go
+++ golang/367-valid-perfect-square.go
@@ -1,23 +1,18 @@
 package golang
 
 func isPerfectSquare(num int) bool {
-	if num == 1 {
-		return true
-	}
-
 	lo, hi := 0, num
 	for lo <= hi {
 		mid := (lo + hi) / 2
-		noRemainder := num%mid == 0
 		square := mid * mid
-		if noRemainder && square == num {
+		if square == num {
 			return true
+		}
+
+		if square < num {
+			lo = mid + 1
 		} else {
-			if square < num {
-				lo = mid + 1
-			} else {
-				hi = mid - 1
-			}
+			hi = mid - 1
 		}
 	}
 	return false
